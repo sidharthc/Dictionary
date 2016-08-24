38f4defaf9c4999ccb5890f5bab0e197711b4ec7
Sidharth Chhawchharia <sidharth.c@hashedin.com>
2016-08-24 13:50:50 +0530

    Made changes to the library  being used

--- hipdict/main.py
+++ hipdict/main.py
@@ -2,17 +2,18 @@ from __future__ import division
 import json
 import requests
 
-
 from flask import render_template, request
 from ac_flask.hipchat import events
 from ac_flask.hipchat.auth import tenant
 
 from hipdict.exceptions import *
 from hipdict import app, addon, tasks
+from PyDictionary import PyDictionary
 
+dictionary = PyDictionary()
 
 # The regular expression for the slash command.
-DICTIONARY_COMMAND_PATTERN = "(^\/[Mm][Ee][Aa][Nn][Ii][Nn][Gg])"
+DICTIONARY_COMMAND_PATTERN = "(^\/[Dd][Ii][Cc][Tt])"
 
 
 @addon.configure_page(path="/configure", methods=['GET'])
@@ -29,7 +30,14 @@ def _render_configuration_page():
     return render_template('configure.html', **model)
 
 
-@app.route('/dicctionary_callback_url', methods=["POST"])
+def construct_meaning(meaning):
+    constructed_meaning = {}
+    for key, value in meaning.iteritems():
+        constructed_meaning[key] = value[0]
+    return constructed_meaning
+
+
+@app.route('/dictionary_callback_url', methods=["POST"])
 @addon.webhook(event="room_message", pattern=DICTIONARY_COMMAND_PATTERN)
 def _handle_dict_callback():
     """This function acts as a HipChat webhook and responds
@@ -39,24 +47,23 @@ def _handle_dict_callback():
     room_id = data['item']['room']['id']
     tenant_id = data['oauth_client_id']
     input_message = data['item']['message']['message']
-    message = "success"
     if len(input_message.split(" ")) > 2:
         message = "Please enter single word to know its meaning."
     elif len(input_message.split(" ")) == 1:
         message = "Usage: /dict <<word>>. It will return you the meaning of the word."
     else:
         word = input_message.split(" ")[1]
-        message = get_meaning_of_the_word(word, tenant_id)
-        list_of_meanings = get_list_of_meanings(message)
-        html_message = "Sorry, We are unable to find meaning of the word <b>'" + word + "'</b>. Please try some other word."
-        if list_of_meanings:
-            data_to_parse = {}
-            data_to_parse["list_of_meanings"] = list_of_meanings
-            data_to_parse["word"] = word
-            html_message = create_html_from_meaning_list(data_to_parse)
-        tasks.send_notification(tenant_id, html_message, room_id, color="random")
+        meaning = dictionary.meaning(word)
+        synonym = dictionary.synonym(word)
+        antonym = dictionary.antonym(word)
+        message = "Sorry, We are unable to find meaning of the word <b>'" + word + "'</b>. Please try some other word."
+        if meaning:
+            meaning = construct_meaning(meaning)
+            synonym = ', '.join(synonym)
+            antonym = ', '.join(antonym)
+            message = create_html_from_meaning_list(word, meaning, synonym, antonym)
     try:
-        tasks.send_notification(tenant_id, message, room_id)
+        tasks.send_notification(tenant_id, message, room_id, color="random")
     except InvalidCredentials as ex:
         app.logger.error(ex)
         return "Invalid Credentials", 401
@@ -71,44 +78,9 @@ def _handle_dict_callback():
     return "Success", 200
 
 
-def create_html_from_meaning_list(data_to_parse):
-        return render_template('message_template.html', **data_to_parse)
-
-def get_list_of_meanings(message):
-    list_of_meanings = []
-    if ('tuc' in message and  len(message['tuc']) > 0 and 
-        'meanings' in message['tuc'][0] and len(message['tuc'][0]['meanings']) >0):
-        for meaning in message['tuc'][0]['meanings']:
-            if len(list_of_meanings) >= 5:
-                break
-            list_of_meanings.append(meaning["text"])
-            
-    return list_of_meanings
-
-
-def get_meaning_of_the_word(word, tenant_id):
-    url = app.config['DICT_BASE_URL'] % (word)
-    resp = requests.request('GET', url, timeout=14)
-    if resp.status_code >= 200 and resp.status_code <= 299:
-        app.logger.info("Generated random room for tenant :"
-        + tenant_id)
-        return resp.json()
-    elif resp.status_code in (400, 404) or\
-    (resp.status_code >= 405 and resp.status_code < 500):
-        app.logger.error('Dict returned %d status code for tenant %s while\
-         sending notification.' % (resp.status_code, str(tenant_id)))
-        raise BadRequest()
-
-    elif resp.status_code >= 500:
-        app.logger.error('Dict returned %d status code for tenant %s while\
-         sending notification.' % (resp.status_code, str(tenant_id)))
-        raise InternalServerError()
-
-    else:
-        app.logger.error('Dict url returned %d status code for tenant %s while \
-        sending notification.' % (resp.status_code, str(tenant_id)))
-        raise Exception('Dict url returned %d status code for tenant %s while\
-         sending notification.' % (resp.status_code, str(tenant_id)))
+def create_html_from_meaning_list(word, meaning, synonym, antonym):
+    data_to_parse = {"meanings": meaning, "synonym": synonym, "antonym": antonym, "word": word}
+    return render_template('message_template.html', **data_to_parse)
 
 
 def on_install(params):
@@ -133,9 +105,9 @@ def page_not_found(error):
     response = {"status": 404,
                 "title": "The page you are trying to access does not exist.",
                 "body": "You may have clicked an old link or mistyped the URL."
-            }
+                }
     return render_template('error.html', msg=response)
-
+    
 
 @app.errorhandler(500)
 def internal_server_error(err):
@@ -144,6 +116,6 @@ def internal_server_error(err):
                 "title": "Internal Server Error!",
                 "body": "The server encountered an internal error or\
                  misconfiguration and was unable to complete your request."
-            }
+                }
     app.logger.error("Internal server error.")
     return render_template('error.html', msg=response)
